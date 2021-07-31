5edd25813f5ab74fbfec19d3af975299378e2f7c
SRUJANA KADAMBARI <saikeerthanakadambari@gmail.com>
2021-07-31 10:29:09 +0530

    firstpython program
    Please enter the commit message for your changes. Lines starting

---
+++ my addition program.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 22 17:53:39 2021
+
+@author: kadambari
+"""
+
+# inputs
+num1 = input('Enter your first value-')
+num2 = input('Enter your second value-')
+ 
+sum = float(num1) + float(num2)
+ 
+print ('the total is {0}'. format( sum ))
+
